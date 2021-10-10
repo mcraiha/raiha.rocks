4f7b99e512a1e4e3e08ac875a31b64a68f363fe9
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2021-10-10 15:45:55 +0300

    Cloudflare pages build
    Maybe

---
+++ cloudflare.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
+chmod +x dotnet-install.sh
+./dotnet-install.sh -c 5.0 -InstallDir ./dotnet5
+./dotnet5/dotnet --version
+./dotnet5/dotnet tool install -g Wyam.Tool
+wyam --recipe Blog --theme CleanBlog
+echo %date% %time% > output/buildinfo.txt
