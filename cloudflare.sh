da7ce113646d37f8e987717626cfbf193e1fe8e6
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2021-10-10 17:39:11 +0300

    Try another DOTNET_ROOT
    50 / 50

--- cloudflare.sh
+++ cloudflare.sh
@@ -3,7 +3,7 @@ DOTNET_CLI_TELEMETRY_OPTOUT=1
 curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh -c 2.1 -InstallDir ./dotnet2
-export DOTNET_ROOT=/opt/buildhome/dotnet2
+export DOTNET_ROOT=/opt/buildhome/repo/dotnet2
 echo $DOTNET_ROOT
 ./dotnet2/dotnet --info
 ./dotnet2/dotnet tool install -g Wyam.Tool
