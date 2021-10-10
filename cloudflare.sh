61dfcf9855b9dfbec750d1783e848f4aca4f4576
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2021-10-10 17:49:15 +0300

    A better buildinfo.txt
    To see when build actually happened

--- cloudflare.sh
+++ cloudflare.sh
@@ -8,4 +8,5 @@ echo $DOTNET_ROOT
 ./dotnet2/dotnet --info
 ./dotnet2/dotnet tool install -g Wyam.Tool
 wyam --recipe Blog --theme CleanBlog
-echo %date% %time% > output/buildinfo.txt
+CURRENTDATE=`date +"%Y-%m-%d %T"`
+echo $CURRENTDATE > output/buildinfo.txt
