b4301144ef5240050946acbbae49f8cc80f178ac
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2021-10-10 16:59:06 +0300

    Check version
    And add DOTNET_ROOT again

--- cloudflare.sh
+++ cloudflare.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 DOTNET_CLI_TELEMETRY_OPTOUT=1
+export DOTNET_ROOT=/opt/buildhome/.dotnet
+dotnet --version
 dotnet tool install -g Wyam.Tool
 wyam --recipe Blog --theme CleanBlog
 echo %date% %time% > output/buildinfo.txt
